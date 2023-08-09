bf3104715a5409979ed910320e9ad0e6882ff3da
RrLSn <afolabisodiqtemitope@yahoo.com>
2023-08-09 19:49:27 +0100

    fixes

--- src/Components/Intro.jsx
+++ src/Components/Intro.jsx
@@ -4,17 +4,55 @@ import Link from 'next/link'
 import Socialmedia from './Socialmedia'
 import { iconLinks } from '@/data'
 import 'aos/dist/aos.css'
+import { motion } from 'framer-motion'
 
 const Intro = () => {
+
+  const variants = {
+    hidden: {
+      opacity : 0,
+      y: "100vh"
+    },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        stiffness: 150,
+        duration: 0.5,
+        when: "beforeChildren"
+      }
+    }
+  }
+
+  const buttonVariants ={
+    hidden: {
+      opacity: 0,
+      y: "10vw"
+    },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {stiffness: 150, duration: 0.6}
+    }
+  }
   
   return (
     <div className={styles.mainWrapper}>
-        <div className={styles.introText}>
+        <motion.div
+        beforeChildren
+        variants={variants}
+        initial="hidden"
+        animate="visible"
+        className={styles.introText}>
           <p>HELLO TO YOU.</p>
           <h1>I&apos;m AFOLABI SODIQ.</h1>
           <h4>FRONTEND-DEVELOPER</h4>
-          <Link href="/PortFolio"><button>View PortFolio</button></Link>
-        </div>
+          <Link href="/PortFolio" className='overflow-hidden'>
+            <motion.button
+            variants={buttonVariants}
+            >View PortFolio</motion.button>
+          </Link>
+        </motion.div>
 
         <div className={styles.introIcon}>
             <div className={styles.icons}>
